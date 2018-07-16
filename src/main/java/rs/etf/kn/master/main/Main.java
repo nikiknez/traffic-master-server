113f2f7855327f211cebf016c8b1070c865653f3
NikLik <NikLik@K55VJ>
2018-07-16 18:09:21 +0200

    Add google maps api key

--- src/main/java/rs/etf/kn/master/main/Main.java
+++ src/main/java/rs/etf/kn/master/main/Main.java
@@ -1,13 +1,18 @@
 package rs.etf.kn.master.main;
 
+import com.google.maps.GeoApiContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import rs.etf.kn.master.dataSource.StreetDataManager;
 import rs.etf.kn.master.dataSource.camera.CamProcessingManager;
 import rs.etf.kn.master.model.Configuration;
 import rs.etf.kn.master.opencv.OpenCV;
 
 public class Main implements ServletContextListener {
 
+    private static String GOOGLE_MAPS_KEY = "AIzaSyCn64R6bJT58MAxEOu9X2rBNdNfB8OjfDM";
+    
+    public static GeoApiContext geoApiContext;
     public Main() {
         System.out.println("Constructor of Main");
     }
@@ -16,7 +21,9 @@ public class Main implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("contextInitialized begin");
         Configuration.load();
-        initializeBackend();
+        StreetDataManager.loadMockMobileData();
+//        initializeBackend();
+        geoApiContext = new GeoApiContext.Builder().apiKey(GOOGLE_MAPS_KEY).build();
         System.out.println("contextInitialized end");
     }
 
