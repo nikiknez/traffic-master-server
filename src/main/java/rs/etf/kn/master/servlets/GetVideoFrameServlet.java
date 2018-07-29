2fa3089ae2c07acf591ca39b272306ec5db76853
NikLik <NikLik@K55VJ>
2018-07-29 13:11:21 +0200

    Add servlet for getting first frame of a video

---
+++ src/main/java/rs/etf/kn/master/servlets/GetVideoFrameServlet.java
@@ -0,0 +1,78 @@
+package rs.etf.kn.master.servlets;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import rs.etf.kn.master.model.Configuration;
+import rs.etf.kn.master.opencv.OpenCV;
+
+public class GetVideoFrameServlet extends HttpServlet {
+
+    private static final Logger LOG = Logger.getLogger(GetVideoFrameServlet.class.getName());
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("image/jpeg;charset=ISO-8859-1");
+        try {
+            String videoFileName = request.getParameter("video");
+            BufferedImage img = OpenCV.readFrame(Configuration.BASE_DIR + videoFileName, 0);
+            ImageIO.write(img, "jpg", response.getOutputStream());
+        } catch (IOException | NullPointerException e) {
+            LOG.severe(e.getMessage());
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
